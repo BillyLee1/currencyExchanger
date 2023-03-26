69225bdc240d69b679e27e2821a432487306ce82
Billy Lee <codingbillylee@gmail.com>
2023-03-25 19:19:59 -0700

    Add initial query selectors for webpage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+let handleConversion = (event) => {
+  event.preventDefault();
+  let userInput = document.querySelector('.userValue').value;
+}
+
+window.addEventListener('load', function() {
+  let change = document.querySelector('.userValue');
+  change.addEventListener('change', handleConversion);
+});
