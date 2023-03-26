648dedb51bb0a0ee793d1b0d6e28cf297e0d6d4f
Billy Lee <codingbillylee@gmail.com>
2023-03-25 20:39:12 -0700

    Add rounding factor

--- src/convert.js
+++ src/convert.js
@@ -2,7 +2,14 @@ import displayConverted from "./index.js";
 
 export default function conversion(response, selection, userInput) {
   if (selection === "cny") {
-    displayConverted(response.conversion_rates.CNY * userInput);
-
+    displayConverted(parseFloat(response.conversion_rates.CNY * userInput).toFixed(2));
+  } else if (selection === "eur") {
+    displayConverted(parseFloat(response.conversion_rates.EUR * userInput).toFixed(2));
+  } else if (selection === "jpy") {
+    displayConverted(parseFloat(response.conversion_rates.JPY * userInput).toFixed(2));
+  } else if (selection === "krw") {
+    displayConverted(parseFloat(response.conversion_rates.KRW * userInput).toFixed(2));
+  } else if (selection === "zar") {
+    displayConverted(parseFloat(response.conversion_rates.ZAR * userInput).toFixed(2));
   }
 }
